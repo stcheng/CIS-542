932b3731ac78f968cd5d7fdb60fd66b501b6bfb2
stcheng <stcheng_89@hotmail.com>
2013-05-22 17:35:52 -0400

    part1 final version
    two threads: one main thread, one extra thread

--- lab01/numbers.c
+++ lab01/numbers.c
@@ -1,123 +1,118 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 
-/* ----- Thread ----- */
-pthread_t inputthread;
-pthread_t printthread;
+/* thread and lock */
+pthread_t thread;
 pthread_mutex_t lock;
 
-/* ----- Parameters -----*/
-int max, min, num, total;
+/* global parameters */
+int max, min, num, total, flag;
 float average;
 int list[5];
 
-/* this function takes the input number */
-void *input(void *arg)
+/* thread function */
+void *print(void *arg)
 {
+	while(flag==0)
+	{
+		sleep(3);
+		pthread_mutex_lock(&lock);
+		if(num==0)
+		{
+			printf("You haven't input anything.\n");
+		}
+		else
+		{
+			printf("The maximum value input so far: %i\n", max);
+			printf("The minimum value input so far: %i\n", min);
+			printf("The average of values input so far: %f\n", average);
+			printf("The last five values input so far: ");
+			int i = (5<num)?5:num;
+			for(--i;i>=0;i--)
+			{
+				printf(" %i", list[i]);
+			}
+			printf("\n");
+		}
+		pthread_mutex_unlock(&lock);
+	}
+	pthread_exit(NULL);
+}
 
+main()
+{
+	
+	if(pthread_mutex_init(&lock, NULL) != 0)
+	{
+		fprintf(stderr, "Error initializing mutex.\n");
+		return 1;
+	}
+	
+	if(pthread_create(&thread, NULL, print, NULL))
+	{
+		fprintf(stderr, "Error creating thread.\n");
+		return 1;
+	}
+    
+	printf("Please input integers, or press 'q' to exit.\n");
+    
+	void *status;
+    
 	while(1)
 	{
-		pthread_mutex_lock(&lock);
-
+		/* user input */
 		char str[80];
 		scanf("%s", str);
-
-		/* if user inputs 'q', program terminates */
+		//fgets(str, 80, stdin);
+        
+		/* if input 'q', program terminates */
 		if(!strcmp(str, "q"))
 		{
+			flag = 1;
+			pthread_join(thread, &status);
 			exit(0);
 		}
-
-		/* if user inputs an integer, save it */
+        
+		/* if input an integer, save it */
 		int temp = atoi(str);
-		if(!temp)
+		char tempstr[80];
+		sprintf(tempstr, "%d", temp);
+		if((!temp && strcmp(str, "0")) || strcmp(str, tempstr))
 		{
 			continue;
 		}
-
+        
 		/* update data */
 		num++;
 		total+=temp;
-
+        
 		/* initialize if it is the first number */
 		if(num == 1)
 		{
 			max = min = temp;
-		} 
-
+		}
+        
 		/* update max, min, average, and recent list */
 		max = (temp > max)?temp:max;
 		min = (temp < min)?temp:min;
 		average = total / (float) num;
-
+        
 		if(num <= 5)
 		{
 			list[num-1] = temp;
 		}
 		else
 		{
-			list[0]=list[1];
-			list[1]=list[2];
-			list[2]=list[3];
-			list[3]=list[4];
-			list[4]=temp;
-		}
-		pthread_mutex_unlock(&lock);
-
-
+			list[0] = list[1];
+			list[1] = list[2];
+			list[2] = list[3];
+			list[3] = list[4];
+			list[4] = temp;
+		}	
 	}
-
-
-}
-
-/* this function sums the number */
-void *print(void *arg)
-{
-	while(1)
-	{
-		sleep(10);
-		pthread_mutex_lock(&lock);
-		printf("The maximum value input so far: %i\n", max);
-		printf("The minimum value input so far: %i\n", min);
-		printf("The average value: %f\n", average);
-		printf("The last five values input:");
-		int i = (5<num)?5:num;
-		for(--i;i>=0;i--)
-		{
-			printf(" %i", list[i]);
-		}
-		printf("\n");
-		pthread_mutex_unlock(&lock);
-	}
-}
-
-int main()
-{
-
-	if (pthread_mutex_init(&lock, NULL) != 0)
-	{
-		fprintf(stderr, "Error initializing mutex.\n");
-		return 1;
-	}
-
-	if(pthread_create(&inputthread, NULL, input, NULL))
-	{
-		fprintf(stderr, "Error creating thread.\n");
-		return 1;
-	}
-	if(pthread_create(&printthread, NULL, print, NULL))
-	{
-		fprintf(stderr, "Error creating thread.\n");
-		return 1;
-	}
-
-	printf("Please input integers, or press 'q' to exit.\n");
-
-	while(1);
-
-	pthread_mutex_destroy(&lock);
-
+    
+	return 0;
+    
+	
 }
