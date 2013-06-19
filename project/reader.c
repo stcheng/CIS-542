c3df2af6d41a3ccec31e383c8c700b1d2390b8b7
stcheng <stcheng_89@hotmail.com>
2013-06-18 20:53:05 -0400

    final version 1.0
    reader.c

--- project/reader.c
+++ project/reader.c
@@ -14,29 +14,30 @@
 
 /* Socket */
 #define MAX_LINE		256
+#define MAX_BUFF		80
 #define MAX_PENDING		10
 #define MAX_THREAD		5
 #define PORT_NUMBER 	8888
 
-// int fd = 0;		// client socket descriptor
+/* Arduino */
+int serial;		// file descriptor of serial port
 
 /* Thread */
 pthread_t socketthread_recv;
 pthread_t socketthread_send;
+pthread_t command_thread;
 pthread_mutex_t lock;
 
-
-
-
-
 int is_sock_running = 0;
+int is_prog_running = 0;
 
 /* parameter */
-char str[80];
+char str[MAX_LINE];
 
 /* Function */
 void *thread_recv(void *arg);
 void *thread_send(void *arg);
+void *thread_command(void *arg);
 
 void *start_server(void *arg)
 {
@@ -85,168 +86,107 @@ void *start_server(void *arg)
 
 	int sin_size = sizeof(struct sockaddr_in);
 
-	while (1  /*  && is_running */)
+	while (1 && is_prog_running )
 	{
+
+		// 4. accept: wait until we get a connection on that port
 		int fd = accept(sock, (struct sockaddr *)&client_addr,(socklen_t *)&sin_size);
-		printf("Server got a connection from (%s, %d)\n", inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+		printf("STATE: Server got a connection from (%s, %d)\n", inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
 
 		is_sock_running = 1;
 		
+		// 5. 6. receive and send information via this socket
 		pthread_create(&socketthread_recv, NULL, thread_recv, (void *)&fd);
 		pthread_create(&socketthread_send, NULL, thread_send, (void *)&fd);
 	}
-	
 
-	// printf("Start receiving.\n");
-	// char buf[MAX_LINE];
-
-
-	// while (recv(fd, buf, sizeof(buf), 0) > 0 && is_sock_running)
-	// {
-	// 	printf("%s\n", buf);
-	// 	if (buf[0] == '$')
-	// 	{
-	// 		printf("quit!!!\n");
-	// 		is_sock_running = 0;
-	// 	}
-	// 	memset(buf, 0, MAX_LINE * sizeof(char));
-	// }		
-    // 4. accept: wait until we get a connection on that port
-    
-    //     	int sin_size = sizeof(struct sockaddr_in);
-    // 	int fd = accept(sock, (struct sockaddr *)&client_addr,(socklen_t *)&sin_size);
-
-    // while(1)
-    // {
-
-	   //  char data[80] = "data\n";
-	   //  send(fd, data, strlen(data), 0);
-	   //  printf("Server sent data.\n");
-	   //  sleep(1);
-    // }
-
-
-
-  //   char buf[80];
-  //   while(1)
-  //   {
-  // //   	if ((fgets(buf, sizeof(buf), stdin)) > 0) {
-		// // 	// buf[79] = '\0';
-		// // 	// // int len = strlen(buf) + 1;
-		// // 	// send(fd, buf, strlen(buf), 0);
-		// // 	// printf("send data.\n");
-		// // 	if (buf == "q\n")
-		// // 	{
-		// // 		printf("break.\n");
-		// // 		break;
-		// // 	}
-		// // 	else
-		// // 	{
-		// // 		printf("wrong.\n");
-		// // 	}
-		// // }
-		// char send_data[80] = "hhhhh";
-		// // sprintf(send_data, "%d", number);
-		// send(fd, send_data, strlen(send_data), 0);
-		// printf("send data.\n");
-		// sleep(1);
-  //   }
-  //   while(1)
-  //   {
-  //   	pthread_mutex_lock(&lock);
-  //   	char send_data[80];
-		// sprintf(send_data, "%d", number);
-	 //    send(fd, send_data, strlen(send_data), 0);
-	 //    pthread_mutex_unlock(&lock);
-  //   }
-    // int i;
-    // for (i = 0; i < 20; i ++)
-    // {
-	   //  char send_data[80] = "here's the message\n";
-	   //  send(fd, send_data, strlen(send_data), 0);    	
-	   //  printf("Send message.\n");
-	   //  sleep(1);
-    // }
-
-
-
-
-//while(count++<10)
-//{	
-
-// pthread_mutex_lock(&lock);
-
-// 	char send_data[80];
-// 	sprintf(send_data, "%d", number);
-
-// pthread_mutex_unlock(&lock);
-
-//     // 6. send: send a message over the socket
-//     send(fd, send_data, strlen(send_data), 0);
-//}	    
-    // printf("Server sent message: %s\n", send_data);
-    
-    // 7. close: close the socket connection
-
-
-
-	
-
+	close(sock);
 }
 
-void *thread_recv(void *arg) {
+void *thread_recv(void *arg)
+{
 	int sock = *((int *) arg);
-	printf("Start receiving from socket: %d\n", sock);
+	printf("STATE: Start receiving from socket: %d\n", sock);
 
-	char buf[MAX_LINE];
+	char buf[MAX_BUFF];
 
 	while (recv(sock, buf, sizeof(buf), 0) > 0 && is_sock_running)
 	{
 		printf("%s\n", buf);
+		if (buf[0] == '%')
+		{
+			if (buf[1] == 'C' && buf[2] == 'L' && buf[3] == 'S')
+			{
+				printf("COMMAND: Close the socket connection.\n");
+				is_sock_running = 0;				
+			}
+		}
 		if (buf[0] == '$')
 		{
-			printf("quit!!!\n");
-			is_sock_running = 0;
+			write(serial, buf, strlen(buf));
 		}
-		memset(buf, 0, MAX_LINE * sizeof(char));
-	}		
+		memset(buf, 0, MAX_BUFF * sizeof(char));
+	}
 
-    printf("Server closed connection\n");
+	// 7. close: close the socket connection
+    printf("STATE: Server closed connection\n");
     close(sock);
+}
+
+void *thread_send(void *arg)
+{
+	int sock = *((int *) arg);
+	memset(str, 0, MAX_LINE * sizeof(char));
+	printf("STATE: Start sending to socket: %d\n", sock);
+
+	while (is_sock_running)
+	{
+		while (str[strlen(str)-1] == '\n')
+		{
+			// printf("send:%s", str);
+			send(sock, str, strlen(str), 0);
+			memset(str, 0, MAX_LINE * sizeof(char));
+		}
+	}
+	
+
 
 }
 
-void *thread_send(void *arg) {
-	int sock = *((int *) arg);
-	printf("Start sending to socket: %d\n", sock);
-
-	char buf[MAX_LINE];
-
-	while (fgets(buf, sizeof(buf), stdin) > 0 && is_sock_running)
+void *thread_command(void *arg)
+{
+	printf("STATE: Start command thread.\n");
+	char command[MAX_LINE];
+	while (fgets(command, sizeof(command), stdin) > 0)
 	{
-		if (buf == "quit")
-			is_sock_running = 0;
-		send(sock, buf, strlen(buf), 0);
-		memset(buf, 0, MAX_LINE * sizeof(char));
+		if (command[0] == '$')
+		{
+			write(serial, command, strlen(command));
+		}
+		else if ((!strcmp(command, "QUIT\n") || (!strcmp(command, "quit\n"))))
+		{
+			printf("STATE: QUIT.\n");
+			is_prog_running = 0;
+			sleep(1);
+			exit(0);
+		}
+		memset(command, 0, MAX_LINE * sizeof(char));
 	}
 
 }
 
 main(int argc, char *argv[])
 {
+	is_prog_running = 1;
+
 	/* connect the device IO */
-	int fd = open("/dev/cu.usbmodem1421", O_RDWR);
-	if(fd == -1)
-	{
-		fprintf(stderr, "Error opening the file.\n");
-		exit(1);
-	}
+	serial = open("/dev/cu.usbmodem1421", O_RDWR);
 
 	struct termios options;
-	tcgetattr(fd, &options);
+	tcgetattr(serial, &options);
 	cfsetispeed(&options, 9600);
 	cfsetospeed(&options, 9600);
-	tcsetattr(fd, TCSANOW, &options);
+	tcsetattr(serial, TCSANOW, &options);
 
 	int port_number;
 	if(argc == 2)
@@ -258,46 +198,29 @@ main(int argc, char *argv[])
 		port_number = 0;
 	}
 
-	/* create thread */
-	if(pthread_mutex_init(&lock, NULL) != 0)
-	{
-		fprintf(stderr, "Error initializing lock.\n");
-		exit(1);
-	}
-
-	if(pthread_create(&socketthread_recv, NULL, start_server, (void*)&port_number))
-	{
-		fprintf(stderr, "Error creating thread.\n");
-		exit(1);
-	}
+	/* create thread & mutex */
+	pthread_mutex_init(&lock, NULL) != 0;
+	pthread_create(&socketthread_recv, NULL, start_server, (void*)&port_number);
+	pthread_create(&command_thread, NULL, thread_command, NULL);
 
 	// while(1);
-	printf("Start reading from arduino.\n");
-
-	// char str[80];
-
+	printf("STATE: Start reading from arduino.\n");
 	while(1)
 	{
-
-		char buf[80];
-		memset(buf, 0, 80 * sizeof(char));
-		int bytes_read = read(fd, buf, 80);
+		char buf[30];
+		memset(buf, 0, 30 * sizeof(char));
+		int bytes_read = read(serial, buf, 40);
 
 		if(bytes_read > 0)
 		{
 			strcat (str, buf);
 
-			if (strlen(str) > 50) 
+			if (strlen(str) > MAX_LINE/5) 
 			{	
-								printf("%s", str);
-				memset(str, 0, 80 * sizeof(char));
+				// printf("%s", str);
+				memset(str, 0, MAX_LINE * sizeof(char));
 			}
 		}
-
-
 	}
-
-
-	
 }
 
